a18c550d350968bcb47334cee90b3be41c9fec9e
M-Kulinkovich <50986910+M-Kulinkovich@users.noreply.github.com>
2020-10-08 00:33:33 +0300

    Create FooterComponent.js

---
+++ app/src/component/Footer/FooterComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import '../../style/footer.scss';
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <div className='footerBox'>
+                <p> MS-4 Group Crew </p>
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
